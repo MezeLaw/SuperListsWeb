c97c5bc4d19d02930d30d9e88d1efcc49b86d505
Meze <mezelaw@gmail.com>
2022-03-08 23:04:32 -0300

    Add animate.css to project.

--- src/app/components/auth/auth-service.service.ts
+++ src/app/components/auth/auth-service.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import "animate.css";
 
 export interface AuthResponse {
   token: string;
