da2bbc57cce603a0d91bbc1ce057e0ae6b279a1f
Meze <mezelaw@gmail.com>
2022-03-16 11:00:35 -0300

    Restyle home and login-error component.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  breakpoint: any;
   constructor() { }
 
   ngOnInit(): void {
+    this.breakpoint = (window.innerWidth <= 400) ? 1 : 3;
+  }
+
+  onResize(event:any) {
+    this.breakpoint = (event.target.innerWidth <= 400) ? 1 : 3;
   }
 
 }
--- src/app/components/login-error/login-error.component.ts
+++ src/app/components/login-error/login-error.component.ts
@@ -1,3 +1,4 @@
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
@@ -7,8 +8,29 @@ import { Router } from '@angular/router';
   styleUrls: ['./login-error.component.css']
 })
 export class LoginErrorComponent implements OnInit {
+  isMobile: boolean = false;
+  public cardColSize=3;
+  public textColSize=1;
+  public textHeight= "500px"; 
 
-  constructor(private router : Router) { }
+  constructor(private router : Router, breakpointObserver: BreakpointObserver) { 
+
+    breakpointObserver.observe([
+      Breakpoints.Handset
+    ]).subscribe(result =>{
+      this.isMobile= result.matches;
+    if(this.isMobile){
+      this.cardColSize=1;
+      this.textColSize = 1;
+      this.textHeight = "625px"; 
+    } else{ 
+        this.cardColSize=4
+        this.textColSize = 1;
+        this.textHeight="150px"; 
+      }	
+     });
+
+  }
 
   ngOnInit(): void {
     console.log("Entre a login-error component")
