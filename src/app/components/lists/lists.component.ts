3f298036e60d1eb3accac3068617ad7c2a045b6b
Meze <mezelaw@gmail.com>
2022-03-20 21:26:18 -0300

    delete list is working.

--- src/app/components/lists/lists.component.ts
+++ src/app/components/lists/lists.component.ts
@@ -1,19 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import {animate, state, style, transition, trigger} from '@angular/animations';
+import { ListsService } from 'src/app/services/lists.service';
 
 export interface List {
-  id: any;
-  Nombre: any;
-  descripcion: any;
-  Codigo: string;
+  //id: any;
+  //Nombre: any;
+  //descripcion: any;
+  //Codigo: string;
+
+  CreatedAt: any;
+  DeletedAt: any;
+  ID: any;
+  UpdatedAt: any;
+  description: any;
+  invite_code: any;
+  list_items: any;
+  name: any;
+  user_creator_id: any
 }
 
-const ELEMENT_DATA: List[] = [
-  {id: 1, Nombre: 'Super', descripcion: "lista del super", Codigo: 'd1ba19dc-940c-4c2a-a2f3-46e1942a7341'},
-  {id: 2, Nombre: 'Laburo', descripcion: "tareas pendientes", Codigo: 'd1b139dc-910c-4c2a-a2f3-46e1942a7341'},
-  {id: 3, Nombre: 'Finde', descripcion: "planes para el finde", Codigo: 'd1bncldc-47x0-4c2a-a2f3-46e1942a7341'},
-  {id: 4, Nombre: 'Rutina Gym', descripcion: "Rutina inicial de gym", Codigo: 'lnmk39dc-910c-4c2a-a2f3-46e1942a7341'}, 
-];
+//const ELEMENT_DATA: List[] = [
+ // {id: 1, Nombre: 'Super', descripcion: "lista del super", Codigo: 'd1ba19dc-940c-4c2a-a2f3-46e1942a7341'},
+  //{id: 2, Nombre: 'Laburo', descripcion: "tareas pendientes", Codigo: 'd1b139dc-910c-4c2a-a2f3-46e1942a7341'},
+  //{id: 3, Nombre: 'Finde', descripcion: "planes para el finde", Codigo: 'd1bncldc-47x0-4c2a-a2f3-46e1942a7341'},
+  //{id: 4, Nombre: 'Rutina Gym', descripcion: "Rutina inicial de gym", Codigo: 'lnmk39dc-910c-4c2a-a2f3-46e1942a7341'}, 
+//];
 
 
 @Component({
@@ -31,15 +42,17 @@ const ELEMENT_DATA: List[] = [
 export class ListsComponent implements OnInit {
 
   columnsToDisplay: string[] = ['Nombre', 'Codigo'];
-  lists : any = []
+  lists : List[] = []
   expandedElement: List | null | undefined;
-  dataSource = ELEMENT_DATA;
+  //dataSource = ELEMENT_DATA;
+  dataSource = this.lists;
   haveLists : boolean = false
 
-  constructor() {
+  constructor(private listService : ListsService) {
 
    //Llamo al servicio del listas
 
+   this.getListForUser()
     
    // Si corresponde las asigno  no y actualizo el valor de haveLists
 
@@ -59,6 +72,31 @@ export class ListsComponent implements OnInit {
 
   deleteList(id : any ) {
     console.log("Ud va a deletear la lista con id: ", id)
+      //this.dataSource = this.dataToDisplay.slice(0, -1);
+   // this.dataSource.setData(this.dataToDisplay);
+
+   //Elimino, si todo sale ok, llamo al request y actualizo tablas
+    this.listService.deleteListByID(id).subscribe(resp => {
+      console.log("list deleted. will refresh the lists ")
+      this.getListForUser()
+
+    }, error => {
+      console.log("error when trying to delete list ")
+    }) 
+
+   
+ 
   }
 
+  getListForUser() {
+    this.listService.getLists().subscribe( response => {
+      console.log("Resultado del get Lists: ", response)
+      this.lists = response
+      this.dataSource = this.lists
+    }, err => {
+      console.log("error al intentar obtener lasl istas", err)
+    })
+  }
+ 
+   
 }
---
+++ src/app/services/lists.service.ts
@@ -0,0 +1,93 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { JwtHelperService } from '@auth0/angular-jwt';
+
+
+export interface List {
+  //id: any;
+  //Nombre: any;
+  //descripcion: any;
+  //Codigo: string;
+
+  CreatedAt: any;
+  DeletedAt: any;
+  ID: any;
+  UpdatedAt: any;
+  description: any;
+  invite_code: any;
+  list_items: any;
+  name: any;
+  user_creator_id: any
+}
+
+export interface DecodedToken {
+  Email : string
+  Role: string 
+  UserID : any
+  //{Email: 'mezequielabogado@gmail.com', Role: 'USER', UserID: 1, exp: 1647512235, iss: 'MezeTheKing'}
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ListsService {
+
+  baseURL = 'http://localhost:8080/v1/lists/'; 
+
+  lists: List[] = [];
+
+  constructor(private http : HttpClient, private jwtHelper: JwtHelperService) { }
+
+
+  getLists() {
+    var token  = localStorage.getItem('token')
+
+    if( token == null ){
+      token = "invalidToken"
+    }
+
+    const decodedToken : DecodedToken = this.jwtHelper.decodeToken(token);
+    
+ 
+    var userId : any = decodedToken.UserID
+
+    console.log("UserID is: ", userId)
+
+    let headers = new HttpHeaders({ "token" : token, "user_id": userId });
+ 
+    return this.http.get<List[]>(`${this.baseURL}`, {headers : headers} );
+  }
+
+
+  getListByID() {
+    var token  = localStorage.getItem('token')
+
+    if( token == null ){
+      token = "invalidToken"
+    } 
+
+    let headers = new HttpHeaders({ "token" : token
+    });
+ 
+    return this.http.get<List>(`${this.baseURL}$/listID`, {headers : headers} );
+  }
+
+  deleteListByID(listID : any) {
+    var token  = localStorage.getItem('token')
+
+    if( token == null ){
+      token = "invalidToken"
+    }
+
+    const decodedToken : DecodedToken = this.jwtHelper.decodeToken(token);
+    
+ 
+    var email : any = decodedToken.Email 
+    var userId : any = decodedToken.UserID
+  
+    let headers = new HttpHeaders({ "token" : token, "user_id": userId });
+ 
+ 
+    return this.http.delete<List>(`${this.baseURL}${listID}`, {headers : headers} );
+  }
+}
